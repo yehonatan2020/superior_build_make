1383b9f8e6457eb443274ada7aa958cc6b32b4c1
yehonatan2020 <yehonatan@yandex.ru>
2024-07-08 10:19:26 +0100

    persist.traced.enable = off

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -445,7 +445,7 @@ PRODUCT_COPY_FILES += system/core/rootdir/init.zygote32.rc:system/etc/init/hw/in
 PRODUCT_VENDOR_PROPERTIES += ro.zygote?=zygote32
 
 PRODUCT_SYSTEM_PROPERTIES += debug.atrace.tags.enableflags=0
-PRODUCT_SYSTEM_PROPERTIES += persist.traced.enable=1
+PRODUCT_SYSTEM_PROPERTIES += persist.traced.enable=0
 
 # Packages included only for eng or userdebug builds, previously debug tagged
 PRODUCT_PACKAGES_DEBUG := \
